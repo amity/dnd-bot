c8c038ca8f350408a0ab818a064761f26ed90773
soph-iest <natejneumann@gmail.com>
2018-12-14 17:43:54 -0500

    made help more readable

--- src/bot.js
+++ src/bot.js
@@ -31,7 +31,7 @@ bot.on("message", function(user, userID, channelID, message, evt) {
     response = `Hi! I'm here to help you play DnD.
     Available commands:
     \`/spell XXX\`: Prints details for spell of name XXX.
-    \`SecretDMcommand XXX\`: Prints details for monster of name XXX.
+    \`/SecretDMcommand XXX\`: Prints details for monster of name XXX.
     \`/help\`: Prints this help menu!
 
     Note that I won't work unless I'm being hosted.

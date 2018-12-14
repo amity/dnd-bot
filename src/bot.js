2bb0848d222ca3e159ee2fc766e5a4925394a076
soph-iest <natejneumann@gmail.com>
2018-12-14 16:09:41 -0500

    monsters init, handling for long descriptions

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ var logger = require("winston");
 var auth = require("../auth.json");
 
 var spellData = require("./spells.json");
+var monsterData = require("./monsters.json");
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -20,49 +21,73 @@ bot.on("ready", function(evt) {
   logger.info("Logged in as: ");
   logger.info(bot.username + " - (" + bot.id + ")");
 });
+
 bot.on("message", function(user, userID, channelID, message, evt) {
+  // TODO: refactor spell and monster out into functions that are called in main one
+
   // Our bot needs to know if it will execute a command
   // It will listen for messages that will start with `!`
   if (message.substring(0, 6) == "/spell") {
-    var spellName = message.substring(7);
-    let mySpell = spellData.find(spell => {
-      return spell.name == spellName;
-    });
-    let requirements;
-    switch (mySpell.components) {
-      case "V":
-        requirements = "Verbal";
-        break;
-      case "S":
-        requirements = "Somatic";
-        break;
-      case "V, S":
-        requirements = "Verbal, Somatic";
-        break;
-      case "V, M":
-        requirements = `Verbal, Material: ${mySpell.material}`;
-        break;
-      case "S, M":
-        requirements = `Somatic, Material: ${mySpell.material}`;
-        break;
-      case "V, S, M":
-        requirements = `Verbal, Somatic, Material: ${mySpell.material}`;
-        break;
-    }
-    requirements = (mySpell.ritual ? "Ritual + " : "") + requirements;
-    const spellMessage = `
-    Spell: ${mySpell.name}
-    Description: ${mySpell.desc}
-    Range: ${mySpell.range}
-    Duration: ${mySpell.duration} ${
-      mySpell.concentration ? ", **Concentrated**" : ""
-    }
-    Cast time: ${mySpell.casting_time}
-    Level: ${mySpell.level} ${mySpell.school} for ${mySpell.class}
-    `;
+    const response = findSpell(message.substring(7));
     bot.sendMessage({
       to: channelID,
-      message: spellMessage
+      message: response
+    });
+  }
+
+  if (message.substring(0, 8) == "/monster") {
+    const monsterName = message.substring(9);
+    let myMonster = monsterData.find(monster => {
+      return monster.name == monsterName;
     });
   }
 });
+
+function findSpell(inputString) {
+  let mySpell = spellData.find(spell => {
+    return spell.name == inputString;
+  });
+  let requirements;
+  switch (mySpell.components) {
+    case "V":
+      requirements = "Verbal";
+      break;
+    case "S":
+      requirements = "Somatic";
+      break;
+    case "V, S":
+      requirements = "Verbal, Somatic";
+      break;
+    case "V, M":
+      requirements = `Verbal, Material: ${mySpell.material}`;
+      break;
+    case "S, M":
+      requirements = `Somatic, Material: ${mySpell.material}`;
+      break;
+    case "V, S, M":
+      requirements = `Verbal, Somatic, Material: ${mySpell.material}`;
+      break;
+  }
+  requirements = (mySpell.ritual ? "Ritual + " : "") + requirements;
+  const spellMessage = `
+    Spell: ${mySpell.name}
+    Description: ${
+      mySpell.desc.length > 1500
+        ? mySpell.desc.substr(0, 1500) + "..."
+        : mySpell.desc
+    }
+    Range: ${mySpell.range}
+    Duration: ${mySpell.duration} ${
+    mySpell.concentration ? ", **Concentrated**" : ""
+  }
+    Cast time: ${mySpell.casting_time}
+    Level: ${mySpell.level} ${mySpell.school} for ${mySpell.class}
+    `;
+  return spellMessage;
+}
+
+function findMonster(inputString) {
+  let myMonster = monsterData.find(monster => {
+    return monster.name == inputString;
+  });
+}

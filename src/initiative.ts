1bd0af63cc5847188aee6a05d8eb55e194c0d6f7
soph-iest <sophie.a.debs@gmail.com>
2019-12-18 15:54:52 +0900

    its been a long time since i did ts and i dont remember importing so im adding monster class code to initiative

--- src/initiative.ts
+++ src/initiative.ts
@@ -41,3 +41,42 @@ export function initiative(channelID: string, inputString: string): string {
 }
 
 
+export class Monster {
+    name: string;
+    type: string;
+    hp: number;
+    status: string = "";
+    initiative: number;
+    constructor(monsterType: string, monsterNum: number = 1, initiative: number = -100) {
+      this.name = monsterType + (monsterNum == undefined ? '' : monsterNum.toString());
+      this.type = monsterType;
+      this.initiative = initiative;
+      this.hp = monsterData.find((monster: any) => {
+        return monster.name == monsterType;
+      }).hit_points;
+    }
+
+    toString(): string {
+      return `${this.name}: ${this.hp} HP ${this.status}
+          `;
+    }
+
+    isType(monsterType: string): boolean {
+      return this.type == monsterType;
+    }
+
+    damage(ticks: number): string {
+      this.hp -= ticks;
+      if (this.hp <= 0) {
+        return `${this.name} has been defeated.
+            `;
+      }
+      return `${this.name} takes ${ticks} damage, and has ${this.hp} HP remaining.
+          `;
+    }
+
+    addStatus(newStatus: string): string {
+      this.status += newStatus;
+      return this.toString();
+    }
+}
